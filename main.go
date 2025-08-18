f8e43ae1722acf6636428cd14978393ce60417d2
Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>
2025-08-18 13:33:12 +0300

    feat(decode): extract important parts from instruction
    Signed-off-by: Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>

--- main.go
+++ main.go
@@ -65,9 +65,26 @@ func fetch(system *System) []byte {
 }
 
 func decode(instruction []byte) (bool, error) {
-	fmt.Printf("Instruction: %x", instruction)
-	switch instruction {
+	fmt.Printf("Instruction: %x\n", instruction)
+
+	firstByte := instruction[0]
+	secondByte := instruction[1]
+
+	firstNibble := firstByte >> 4
+	secondNibble := firstByte & 0x0F
+	thirdNibble := secondByte >> 4
+	fourthNibble := secondByte & 0x0F
+
+	last3Nibbles := (uint16(secondNibble) << 8) | (uint16(thirdNibble) << 4) | uint16(fourthNibble)
+
+	fmt.Printf("Bytes: %x%x\n", firstByte, secondByte)
+	fmt.Printf("nibbles: %x%x%x%x\n", firstNibble, secondNibble, thirdNibble, fourthNibble)
+	fmt.Printf("last 3: %x\n", last3Nibbles)
+
+	switch firstNibble {
+
 	}
+
 	return true, nil
 }
 
