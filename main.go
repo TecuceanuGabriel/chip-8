57e18289b825da5638618c04ab50bf679e484f58
Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>
2025-08-20 20:18:22 +0300

    fix: restore terminal after exiting fetch-decode loop
    Signed-off-by: Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>

--- main.go
+++ main.go
@@ -15,6 +15,9 @@ func main() {
 
 	system := system.CreateSystem()
 
+	initTerminal()
+	defer restoreTerminal()
+
 	for {
 		instruction := system.Fetch()
 		exit, err := system.Decode(instruction)
@@ -26,4 +29,16 @@ func main() {
 			break
 		}
 	}
+}
+
+func initTerminal() {
+	fmt.Print("\x1b[?25l") // hide cursor
+}
+
+func restoreTerminal() {
+	// TODO: print only once
+	fmt.Print("\x1b[0m")   // reset all attributes (colors, styles)
+	fmt.Print("\x1b[?25h") // show cursor
+	fmt.Print("\x1b[2J")   // clear screen
+	fmt.Print("\x1b[H")    // move cursor to home position
 }
