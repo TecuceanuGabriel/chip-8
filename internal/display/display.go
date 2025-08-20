abf69182e29f9c8f6d04c54cb432a9f3a4613129
Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>
2025-08-20 18:36:48 +0300

    fix: proper sprite indexing in DrawSprite()
    - before the sprite was drawn in reverse
    
    Signed-off-by: Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>

--- internal/display/display.go
+++ internal/display/display.go
@@ -21,30 +21,48 @@ func GetTerminalSize() (width, height int, err error) {
 	return term.GetSize(fd)
 }
 
-func ClearScreen() {
+func (d *Display) ClearScreen() {
 	fmt.Print("\x1b[2J\x1b[H")
-}
-
-func (d *Display) drawCell(fill bool, pos_x, pos_y int) {
-	idx := pos_x + pos_y*width
-	if d.pixels[idx] != fill {
-		d.pixels[idx] = !d.pixels[idx]
-		color := "0"
-		if d.pixels[idx] {
-			color = "255"
+	for i := range height {
+		for j := range width {
+			d.pixels[j+i*width] = false
 		}
-		fmt.Printf("\x1b[%v;%vH", pos_y, pos_x)
-		fmt.Printf("\x1b[48;5;%vm", color)
-		fmt.Print(" ")
-		fmt.Print("\x1b[0m") // reset
 	}
 }
 
-func (d *Display) DrawSprite(sprite []byte, pos_x, pos_y, n int) (err error) {
+func (d *Display) drawCell(fill bool, pos_x, pos_y byte) (erasing bool) {
+	erasing = false
+	idx := pos_x + pos_y*width
+
+	if fill == true {
+		var color string
+		if d.pixels[idx] == true {
+			d.pixels[idx] = false
+			color = "255"
+			erasing = true
+		} else {
+			d.pixels[idx] = true
+			color = "0"
+		}
+
+		// TODO: use one print
+		fmt.Print("\x1b[?25l") // hide cursor
+		fmt.Printf("\x1b[%v;%vH", pos_y+1, pos_x+1)
+		fmt.Printf("\x1b[48;5;%vm", color)
+		fmt.Print("█")
+		fmt.Print("\x1b[0m") // reset
+	}
+
+	return erasing
+}
+
+func (d *Display) DrawSprite(sprite []byte, pos_x, pos_y, n byte) (erasing bool, err error) {
+	erasing = false
+
 	tWidth, tHeight, err := GetTerminalSize()
 	if err != nil {
 		fmt.Println(err)
-		return err
+		return erasing, err
 	}
 
 	if tWidth < width || tHeight < height {
@@ -52,16 +70,20 @@ func (d *Display) DrawSprite(sprite []byte, pos_x, pos_y, n int) (err error) {
 		os.Exit(1)
 	}
 
+	// wrap coordinates
 	pos_x = pos_x % width
 	pos_y = pos_y % height
 
 	for i := range n {
 		line := sprite[i]
-		for j := range 8 {
-			fill := (line>>j)&1 == 1
-			d.drawCell(fill, pos_x+j, pos_y+i)
+		for j := range byte(8) {
+			fill := (line>>(7-j))&1 == 1
+			e := d.drawCell(fill, pos_x+j, pos_y+i)
+			if e {
+				erasing = e
+			}
 		}
 	}
 
-	return nil
+	return erasing, nil
 }
